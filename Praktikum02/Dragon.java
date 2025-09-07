91b716dd6be5c2d0e89e1a2b10639c0c1d63f19d
Fizzrss <fizars123@gmail.com>
2025-09-07 16:33:58 +0700

    first commit

---
+++ Praktikum02/Dragon.java
@@ -0,0 +1,64 @@
+public class Dragon {
+    int x;
+    int y;
+    int direction;
+
+    Dragon(int awalX, int awalY, int awalDirection){
+        x = awalX;
+        y = awalY;
+        direction = awalDirection;
+    }
+
+    void changeDirection(int newDirection){
+        if (newDirection >= 1 && newDirection <= 4) {
+            this.direction = newDirection;
+        } else {
+            System.out.println("Input tidak sesuai. Gunakan angka \n1:atas\n2:kanan\n3:bawah\n4:kiri");
+        }
+    }
+
+    void move(int steps){
+        switch (this.direction) {
+            case 1:
+                this.y += steps;
+                break;
+            case 2:
+                this.x += steps;
+                break;
+            case 3:
+                this.y -= steps;
+                break;
+            case 4:
+                this.x -= steps;
+                break;
+            default:
+                System.out.println("Arah belum diatur");
+                break;
+        }
+    }
+
+    void printStatus(){
+        String statusDirection;
+        switch (this.direction) {
+            case 1:
+                statusDirection = "Atas";
+                break;
+            case 2:
+                statusDirection = "Kanan";
+                break;
+            case 3:
+                statusDirection = "Bawah";
+                break;
+            case 4:
+                statusDirection = "Kiri";
+                break;
+            default:
+                statusDirection = "Tidak diketahui";
+                break;
+        }
+
+        System.out.println("Koordinat\t: " + "(" + this.x + "," + this.y + ")");
+        System.out.println("Arah\t\t: " + statusDirection);
+        System.out.println("====================");
+    }
+}
---
+++ Praktikum02/DragonMain.java
@@ -0,0 +1,42 @@
+public class DragonMain {
+    public static void main(String[] args) {
+        System.out.println("===== Informasi DIrection Dragon =====");
+        System.out.println("1 = Atas");
+        System.out.println("2 = Kanan");
+        System.out.println("3 = Bawah");
+        System.out.println("4 = Kiri");
+        System.out.println("======================================");
+
+        
+        
+        System.out.println("\n========== Dragon 1 ==========");
+        System.out.println("---- Status Awal ----");
+        Dragon dragon1 = new Dragon(0, 0, 1);
+        dragon1.printStatus();
+        System.out.println("---- Bergerak 2 langkah ----");
+        dragon1.move(2);
+        dragon1.printStatus();
+        System.out.println("---- Berubah arah ke bawah ----");
+        dragon1.changeDirection(3);
+        dragon1.printStatus();
+        System.out.println("---- Bergerak 4 langkah ----");
+        dragon1.move(4);
+        dragon1.printStatus();
+        System.out.println("---- Berubah arah ke kiri dan bergerak 3 langkah ----");
+        dragon1.changeDirection(4);
+        dragon1.move(3);
+        dragon1.printStatus();
+        
+        System.out.println("\n========== Dragon 2 ==========");
+        System.out.println("---- Status Awal ----");
+        Dragon dragon2 = new Dragon(3, 2, 2);
+        dragon2.printStatus();
+        System.out.println("---- Bergerak 5 langkah ----");
+        dragon2.move(5);
+        dragon2.printStatus();
+        System.out.println("---- Berubah arah ke atas dan bergerak 3 langkah ----");
+        dragon2.changeDirection(1);
+        dragon2.move(3);
+        dragon2.printStatus();
+    }
+}
