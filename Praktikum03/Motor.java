9a1ed58f9888447e0aed2f7d1dfd4d58dcf42689
Fizzrss <fizars123@gmail.com>
2025-09-11 09:54:21 +0700

    percobaan2modifno5

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -30,6 +30,9 @@ public class Motor {
             if (kecepatan >= 100) {
                 System.out.println("Kecepatan Maksimal adalah 100");
                 this.kecepatan = 100;
+            } else if (kecepatan < 0) {
+                System.out.println("Kecepatan tidak boleh negatif");
+                this.kecepatan = 0;
             } else {
                 this.kecepatan = kecepatan;
             }
