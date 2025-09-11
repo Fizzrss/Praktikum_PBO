83c2f8b58e275d9edca6609a66b49a38accbbd9c
Fizzrss <fizars123@gmail.com>
2025-09-11 11:07:58 +0700

    tugas

---
+++ Praktikum03/Anggota.java
@@ -0,0 +1,42 @@
+public class Anggota {
+    private String nomorKTP;
+    private String nama;
+    private int limitPeminjaman;
+    private int jumlahPinjaman;
+
+    public Anggota(String nomorKTP, String nama, int limitPeminjaman){
+        this.nomorKTP = nomorKTP;
+        this.nama = nama;
+        this.limitPeminjaman = limitPeminjaman;
+        this.jumlahPinjaman = 0;
+    }
+
+    public String getNomorKTP() {
+        return nomorKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getLimitPeminjaman() {
+        return limitPeminjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(int uang){
+        if (jumlahPinjaman + uang > limitPeminjaman) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit");
+        } else {
+            jumlahPinjaman += uang;
+        }
+    }
+
+    public void angsur(int uang){
+        jumlahPinjaman -= uang;
+    }
+
+}
