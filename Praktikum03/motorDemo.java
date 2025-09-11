9c2262a2505e4f0c59ffaee573aeee454433ea22
Fizzrss <fizars123@gmail.com>
2025-09-11 08:42:58 +0700

    percobaan1tanpaenkapsulasi

---
+++ Praktikum03/motorDemo.java
@@ -0,0 +1,8 @@
+public class motorDemo {
+    public static void main(String[] args) {
+        Motor motor1 = new Motor();
+        motor1.platNomor = "B 0838 XZ";
+        motor1.kecepatan = 50;
+        motor1.displayInfo();
+    }
+}
