4773de154f081e2c067d9410f0ad2135043f448c
Fizzrss <fizars123@gmail.com>
2025-10-09 09:34:35 +0700

    percobaan5

--- Praktikum07/Dosen.java
+++ Praktikum07/Dosen.java
@@ -6,9 +6,13 @@ public class Dosen extends Pegawai {
         System.out.println("Objek dari class Dosen dibuat");
     }   
 
+    public String getInfo(){
+        return "NIDN        : " + this.nidn + "\n";
+    }
+
     public String getAllInfo(){
-        String info = this.getInfo();
-        info += "NIDN      : " + nidn;
+        String info = super.getInfo();
+        info += this.getInfo();
 
         return info;
     }
