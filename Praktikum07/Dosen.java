f38a5cf325aa63d971a1ff4790c63fbba14fefed
Fizzrss <fizars123@gmail.com>
2025-10-09 09:13:40 +0700

    percobaan3_testpackage

--- Praktikum07/Dosen.java
+++ Praktikum07/Dosen.java
@@ -1,8 +1,10 @@
+import testpackage.Pegawai;
+
 public class Dosen extends Pegawai {
     public String nidn;
 
     public Dosen() {
+        System.out.println(gaji);
         System.out.println("Objek dari class Dosen dibuat");
-    }
-    
+    }   
 }
