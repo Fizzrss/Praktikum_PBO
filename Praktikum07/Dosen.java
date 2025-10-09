8301af3c87dce69e501c3df6c868973f8cf0ba6c
Fizzrss <fizars123@gmail.com>
2025-10-09 09:49:20 +0700

    percobaan6

--- Praktikum07/Dosen.java
+++ Praktikum07/Dosen.java
@@ -2,8 +2,11 @@ public class Dosen extends Pegawai {
     public String nidn;
 
     public Dosen() {
-        System.out.println(gaji);
         System.out.println("Objek dari class Dosen dibuat");
+    }
+
+    public Dosen(String nip, String nama, double gaji, String nidn) {
+        System.out.println("Objek dari class Dosen dibuat dengan construktor berparameter");
     }   
 
     public String getInfo(){
