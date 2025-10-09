c1fdcdf4fd001c108e4df48eb4412404c59baf4d
Fizzrss <fizars123@gmail.com>
2025-10-09 10:26:43 +0700

    percobaan7

--- Praktikum07/Dosen.java
+++ Praktikum07/Dosen.java
@@ -1,17 +1,16 @@
 public class Dosen extends Pegawai {
     public String nidn;
 
-    public Dosen() {
-        System.out.println("Objek dari class Dosen dibuat");
-        super(nip, nama, gaji);
-    }
-
+    // public Dosen() {
+    //     System.out.println("Objek dari class Dosen dibuat");
+    // }
+    
     public Dosen(String nip, String nama, double gaji, String nidn) {
-        super();
+        this.nidn = nidn;
+        super(nip, nama, gaji);
         super.nip = nip;
         super.nama = nama;
         super.gaji = gaji;
-        this.nidn = nidn;
     }   
 
     public String getInfo(){
