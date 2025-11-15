95f55425f6faac6d2d472e1ada9bcc962f60c6bb
Fizzrss <fizars123@gmail.com>
2025-11-15 14:44:21 +0700

    PolymorphicArguments

--- Praktikum10/Demo.java
+++ Praktikum10/Demo.java
@@ -1,19 +1,14 @@
-import java.util.ArrayList;
-
 public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.kom", "199402");
+        TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
 
-        Pegawai pegawai1 = dosen1;
+        train(dosen1);
+        train(tendik1);
 
-        System.out.println(pegawai1.nip);
-        System.out.println(pegawai1.nama);
-        pegawai1.displayInfo();
-
-        Dosen newDosen = (Dosen) pegawai1;
-
-        System.out.println(newDosen.nama);
-        System.out.println(newDosen.nidn);
-        newDosen.mengajar();
+    }
+    public static void train(Pegawai pegawai) {
+        System.out.println("Memberikan pelatihan untuk pegawai");
+        pegawai.displayInfo();
     }
 }
