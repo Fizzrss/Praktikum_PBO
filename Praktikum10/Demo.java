2594bc1db0f18b74ab02bb7289b9e5c48bcc52a9
Fizzrss <fizars123@gmail.com>
2025-11-15 14:20:47 +0700

    ObjectCasting - upcasting modif

--- Praktikum10/Demo.java
+++ Praktikum10/Demo.java
@@ -8,7 +8,6 @@ public class Demo {
 
         System.out.println(pegawai1.nip);
         System.out.println(pegawai1.nama);
-        System.out.println(pegawai1.nidn);
-        pegawai1.mengajar();
+        pegawai1.displayInfo();
     }
 }
