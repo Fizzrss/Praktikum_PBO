04e20e30c72729990060d19a141640e5e7d218dc
Fizzrss <fizars123@gmail.com>
2025-11-19 07:34:34 +0700

    downcasting pegawai ke dosen

--- Praktikum10/Demo.java
+++ Praktikum10/Demo.java
@@ -3,6 +3,10 @@ public class Demo {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.kom", "199402");
         TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
 
+        // System.out.println("=== Donwcasting Pegawai ke Dosen ===");
+        // Pegawai pegawai1 = new Pegawai("20000101", "Budi, S.Pd.");
+        // Dosen dosen2 = (Dosen) pegawai1;
+
         train(dosen1);
         train(tendik1);
 
